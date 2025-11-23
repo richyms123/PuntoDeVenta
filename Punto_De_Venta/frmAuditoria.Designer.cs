93777cf512bb7b36613f7fce0627a4894ca4d41d
Richy <richysalgado123@gmail.com>
2025-11-23 13:05:51 -0600

    Diseño: Módulo de auditorías finalizado con sus respectivos controles de usuario.

---
+++ Punto_De_Venta/frmAuditoria.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Punto_De_Venta
+{
+    partial class frmAuditoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAuditoria));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bunifuPanel1 = new Bunifu.UI.WinForms.BunifuPanel();
+            this.pnlContenedor = new System.Windows.Forms.Panel();
+            this.materialPictureBox2 = new FontAwesome.Sharp.Material.MaterialPictureBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.ctlAuditoria1 = new Punto_De_Venta.ControlesUsuario.ctlAuditoria();
+            this.ctlTituloAuditoria1 = new Punto_De_Venta.ControlesUsuario.ctlTituloAuditoria();
+            this.ctlAuditoria2 = new Punto_De_Venta.ControlesUsuario.ctlAuditoria();
+            this.ctlAuditoria3 = new Punto_De_Venta.ControlesUsuario.ctlAuditoria();
+            this.ctlAuditoria4 = new Punto_De_Venta.ControlesUsuario.ctlAuditoria();
+            this.ctlAuditoria5 = new Punto_De_Venta.ControlesUsuario.ctlAuditoria();
+            this.ctlAuditoria6 = new Punto_De_Venta.ControlesUsuario.ctlAuditoria();
+            this.ctlAuditoria7 = new Punto_De_Venta.ControlesUsuario.ctlAuditoria();
+            this.ctlAuditoria8 = new Punto_De_Venta.ControlesUsuario.ctlAuditoria();
+            this.panel1.SuspendLayout();
+            this.bunifuPanel1.SuspendLayout();
+            this.pnlContenedor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.materialPictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.bunifuPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1168, 712);
+            this.panel1.TabIndex = 1;
+            // 
+            // bunifuPanel1
+            // 
+            this.bunifuPanel1.AutoSize = true;
+            this.bunifuPanel1.BackgroundColor = System.Drawing.Color.White;
+            this.bunifuPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuPanel1.BackgroundImage")));
+            this.bunifuPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuPanel1.BorderColor = System.Drawing.Color.Transparent;
+            this.bunifuPanel1.BorderRadius = 20;
+            this.bunifuPanel1.BorderThickness = 1;
+            this.bunifuPanel1.Controls.Add(this.pnlContenedor);
+            this.bunifuPanel1.Controls.Add(this.materialPictureBox2);
+            this.bunifuPanel1.Controls.Add(this.lblUsuario);
+            this.bunifuPanel1.Location = new System.Drawing.Point(27, 26);
+            this.bunifuPanel1.Name = "bunifuPanel1";
+            this.bunifuPanel1.ShowBorders = true;
+            this.bunifuPanel1.Size = new System.Drawing.Size(1100, 651);
+            this.bunifuPanel1.TabIndex = 4;
+            // 
+            // pnlContenedor
+            // 
+            this.pnlContenedor.AutoSize = true;
+            this.pnlContenedor.BackColor = System.Drawing.Color.White;
+            this.pnlContenedor.Controls.Add(this.ctlAuditoria8);
+            this.pnlContenedor.Controls.Add(this.ctlAuditoria7);
+            this.pnlContenedor.Controls.Add(this.ctlAuditoria6);
+            this.pnlContenedor.Controls.Add(this.ctlAuditoria5);
+            this.pnlContenedor.Controls.Add(this.ctlAuditoria4);
+            this.pnlContenedor.Controls.Add(this.ctlAuditoria3);
+            this.pnlContenedor.Controls.Add(this.ctlAuditoria2);
+            this.pnlContenedor.Controls.Add(this.ctlAuditoria1);
+            this.pnlContenedor.Controls.Add(this.ctlTituloAuditoria1);
+            this.pnlContenedor.Location = new System.Drawing.Point(18, 98);
+            this.pnlContenedor.Name = "pnlContenedor";
+            this.pnlContenedor.Size = new System.Drawing.Size(1055, 550);
+            this.pnlContenedor.TabIndex = 10;
+            // 
+            // materialPictureBox2
+            // 
+            this.materialPictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.materialPictureBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(39)))), ((int)(((byte)(35)))));
+            this.materialPictureBox2.IconChar = FontAwesome.Sharp.MaterialIcons.TelevisionGuide;
+            this.materialPictureBox2.IconColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(39)))), ((int)(((byte)(35)))));
+            this.materialPictureBox2.IconSize = 55;
+            this.materialPictureBox2.Location = new System.Drawing.Point(18, 15);
+            this.materialPictureBox2.Name = "materialPictureBox2";
+            this.materialPictureBox2.Size = new System.Drawing.Size(55, 55);
+            this.materialPictureBox2.TabIndex = 6;
+            this.materialPictureBox2.TabStop = false;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lblUsuario.Font = new System.Drawing.Font("Segoe UI Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(39)))), ((int)(((byte)(35)))));
+            this.lblUsuario.Location = new System.Drawing.Point(73, 20);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(159, 41);
+            this.lblUsuario.TabIndex = 3;
+            this.lblUsuario.Text = "Auditoría";
+            // 
+            // ctlAuditoria1
+            // 
+            this.ctlAuditoria1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ctlAuditoria1.Location = new System.Drawing.Point(0, 50);
+            this.ctlAuditoria1.Name = "ctlAuditoria1";
+            this.ctlAuditoria1.Size = new System.Drawing.Size(1055, 50);
+            this.ctlAuditoria1.TabIndex = 1;
+            // 
+            // ctlTituloAuditoria1
+            // 
+            this.ctlTituloAuditoria1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(39)))), ((int)(((byte)(35)))));
+            this.ctlTituloAuditoria1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ctlTituloAuditoria1.Location = new System.Drawing.Point(0, 0);
+            this.ctlTituloAuditoria1.Name = "ctlTituloAuditoria1";
+            this.ctlTituloAuditoria1.Size = new System.Drawing.Size(1055, 50);
+            this.ctlTituloAuditoria1.TabIndex = 0;
+            // 
+            // ctlAuditoria2
+            // 
+            this.ctlAuditoria2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ctlAuditoria2.Location = new System.Drawing.Point(0, 100);
+            this.ctlAuditoria2.Name = "ctlAuditoria2";
+            this.ctlAuditoria2.Size = new System.Drawing.Size(1055, 50);
+            this.ctlAuditoria2.TabIndex = 2;
+            // 
+            // ctlAuditoria3
+            // 
+            this.ctlAuditoria3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ctlAuditoria3.Location = new System.Drawing.Point(0, 150);
+            this.ctlAuditoria3.Name = "ctlAuditoria3";
+            this.ctlAuditoria3.Size = new System.Drawing.Size(1055, 50);
+            this.ctlAuditoria3.TabIndex = 3;
+            // 
+            // ctlAuditoria4
+            // 
+            this.ctlAuditoria4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ctlAuditoria4.Location = new System.Drawing.Point(0, 200);
+            this.ctlAuditoria4.Name = "ctlAuditoria4";
+            this.ctlAuditoria4.Size = new System.Drawing.Size(1055, 50);
+            this.ctlAuditoria4.TabIndex = 4;
+            // 
+            // ctlAuditoria5
+            // 
+            this.ctlAuditoria5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ctlAuditoria5.Location = new System.Drawing.Point(0, 250);
+            this.ctlAuditoria5.Name = "ctlAuditoria5";
+            this.ctlAuditoria5.Size = new System.Drawing.Size(1055, 50);
+            this.ctlAuditoria5.TabIndex = 5;
+            // 
+            // ctlAuditoria6
+            // 
+            this.ctlAuditoria6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ctlAuditoria6.Location = new System.Drawing.Point(0, 300);
+            this.ctlAuditoria6.Name = "ctlAuditoria6";
+            this.ctlAuditoria6.Size = new System.Drawing.Size(1055, 50);
+            this.ctlAuditoria6.TabIndex = 6;
+            // 
+            // ctlAuditoria7
+            // 
+            this.ctlAuditoria7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ctlAuditoria7.Location = new System.Drawing.Point(0, 350);
+            this.ctlAuditoria7.Name = "ctlAuditoria7";
+            this.ctlAuditoria7.Size = new System.Drawing.Size(1055, 50);
+            this.ctlAuditoria7.TabIndex = 7;
+            // 
+            // ctlAuditoria8
+            // 
+            this.ctlAuditoria8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ctlAuditoria8.Location = new System.Drawing.Point(0, 400);
+            this.ctlAuditoria8.Name = "ctlAuditoria8";
+            this.ctlAuditoria8.Size = new System.Drawing.Size(1055, 50);
+            this.ctlAuditoria8.TabIndex = 8;
+            // 
+            // frmAuditoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1168, 712);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmAuditoria";
+            this.Text = "frmAuditoria";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.bunifuPanel1.ResumeLayout(false);
+            this.bunifuPanel1.PerformLayout();
+            this.pnlContenedor.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.materialPictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Bunifu.UI.WinForms.BunifuPanel bunifuPanel1;
+        private System.Windows.Forms.Panel pnlContenedor;
+        private FontAwesome.Sharp.Material.MaterialPictureBox materialPictureBox2;
+        private System.Windows.Forms.Label lblUsuario;
+        private ControlesUsuario.ctlAuditoria ctlAuditoria1;
+        private ControlesUsuario.ctlTituloAuditoria ctlTituloAuditoria1;
+        private ControlesUsuario.ctlAuditoria ctlAuditoria8;
+        private ControlesUsuario.ctlAuditoria ctlAuditoria7;
+        private ControlesUsuario.ctlAuditoria ctlAuditoria6;
+        private ControlesUsuario.ctlAuditoria ctlAuditoria5;
+        private ControlesUsuario.ctlAuditoria ctlAuditoria4;
+        private ControlesUsuario.ctlAuditoria ctlAuditoria3;
+        private ControlesUsuario.ctlAuditoria ctlAuditoria2;
+    }
+}
---
+++ Punto_De_Venta/frmAuditoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto_De_Venta
+{
+    public partial class frmAuditoria : Form
+    {
+        public frmAuditoria()
+        {
+            InitializeComponent();
+        }
+    }
+}
