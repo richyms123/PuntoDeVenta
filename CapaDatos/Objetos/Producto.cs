1eb66849ae7db41c9618db6afcb2485c86a6f196
Andre Garcia <garciajoseph537@gmail.com>
2025-11-23 14:34:11 -0600

    Objetos relacionados a la base de datos

---
+++ CapaDatos/Objetos/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaDatos.Objetos
+{
+    internal class Producto
+    {
+        public int idProducto { get; set; }
+        public string Nombre { get; set; }
+        public string CantidadPorUnidad { get; set; }
+        //DECIMAL
+        public float PrecioUnitario { get; set; }
+        public int Stock { get; set; }
+        public bool Descontinuado { get; set; }
+        public string FotoProducto { get; set; }
+        public int idCategoria { get; set; }
+
+    }
+}
